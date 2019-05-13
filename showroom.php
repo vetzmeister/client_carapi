e8abf01841e3bbf1683eec4d71e0e024a8b08035
Niclas Vetter <vetzmeister@hotmail.com>
2019-05-13 13:21:00 +0200

    showroom

--- showroom.php
+++ showroom.php
@@ -5,22 +5,100 @@ $keyWords = 'showroom page client car api';
 
 $page_id = 'showroom';
 require_once('partials/_nav.php');
+
 ?>
-    <section class="row">
+
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="bg-img2 col-12">
+                <form action="/action_page.php" class="container">
+                    <h4>Audi RS6 Avant</h4>
+
+                    <label for="Specs"><b>Specifications</b></label>
+                    <ul>
+                        <li>HK: 450 </li>
+                        <li>NM: 800</li>
+                        <li>Km/L: 4.5</li>
+                        <li>Km: 3000</li>
+                    </ul>
+
+                    <button type="button" class="btn">Read More</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="bg-img1 col-12">
+                <form action="/action_page.php" class="container">
+                    <h4>Audi RS6 Avant</h4>
+
+                    <label for="Specs"><b>Specifications</b></label>
+                    <ul>
+                        <li>HK: 450 </li>
+                        <li>NM: 800</li>
+                        <li>Km/L: 4.5</li>
+                        <li>Km: 3000</li>
+                    </ul>                
+
+                    <button type="button" class="btn">Read More</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- IMAGE TEST -->
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="bg-img2 col-12">
+                <form action="/action_page.php" class="container">
+                    <h4>Audi RS6 Avant</h4>
+
+                    <label for="Specs"><b>Specifications</b></label>
+                    <ul>
+                        <li>HK: 450 </li>
+                        <li>NM: 800</li>
+                        <li>Km/L: 4.5</li>
+                        <li>Km: 3000</li>
+                    </ul>
+
+                    <button type="button" class="btn">Read More</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="row">   
+    <div class="col-6">
+        <img class="card-img-top" src="img/audi/audi3.jpg" alt="Audi Car">
+    </div>    
+    <div class="col-2">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>     
+</section>
+
+<section class="row">
         <div class="col-6">
             <img class="card-img-top" src="img/audi/audi3.jpg" alt="Audi Car">
+        </div>   
+
+        <div class="col-2">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
-        <div class="col-6">
-            <img class="card-img-top" src="img/audi/audi3.jpg" alt="Audi Car">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </section>
+</section>

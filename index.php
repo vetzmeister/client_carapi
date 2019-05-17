c4f46edd15158bbdf6dde5c8b4596c6b9edb77a2
reeb b <isusnavi2005@gmail.com>
2019-05-17 21:53:30 +0200

    auth and sessions working

--- index.php
+++ index.php
@@ -1,26 +1,18 @@
 <?php
 include ('partials/_head.php');
-
+include ('partials/_nav.php');
 ?>
 
 
 
 <body>
     <?php
-       include ('partials/_nav.php');
-
-        if (isset($_GET['whatever'])){
-            // include ('whatever.php');
-            echo "whatever";
-        }
-        elseif (isset($_GET['showroom'])){
-        include ('showroom.php');
-
+        if (isset($_GET['showroom'])){
+            include ('showroom.php'); 
         }
         elseif (isset($_GET['home'])){
-        include ('home.php'); 
-        }
-
+            include ('home.php'); 
+        } 
         include ('partials/_footer.php');
     ?>
 </body>
